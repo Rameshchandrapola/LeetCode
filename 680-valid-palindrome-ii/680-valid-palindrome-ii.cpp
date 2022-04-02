8ca4d93eb969c497e5a067e6e81da54d043236c1
Ramesh chandra pola <76244600+Rameshchandrapola@users.noreply.github.com>
2022-04-02 15:43:45 +0530

    Time: 60 ms (80.88%), Space: 19.6 MB (71.13%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -12,8 +12,10 @@ public:
         if(i>j){
             return true;
         }
+        
         int i_1=i+1;
         int j_1=j-1;
+        
         while(i_1<=j&&s[i_1]==s[j]){
             i_1++;
             j--;
@@ -21,16 +23,17 @@ public:
         if(i_1>j){
             return true;
         }
+        
         while(i<=j_1&&s[i]==s[j_1]){
 
                 j_1--;
                 i++;
             
         }
-        
         if(i>j_1){
             return true;
         }
+        
         return false;
     }
 };
