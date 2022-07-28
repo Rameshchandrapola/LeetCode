4ca2e4da352ee7c3443145493353fbb65fc55450
Ramesh chandra pola <76244600+Rameshchandrapola@users.noreply.github.com>
2022-07-28 21:50:38 +0530

    Time: 10 ms (51.58%), Space: 9.1 MB (22.53%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void fns(vector<vector<int>>&dp,int j, int n,vector<vector<int>>triangle){
+    void fns(int j, int n,vector<vector<int>>&triangle){
         for(j=1;j<n;j++)
         {
             for(int i=0;i<j+1;i++)
             {
                 if(i==0)
                 {
-                    dp[j][i]=dp[j-1][i]+triangle[j][i];
+                    triangle[j][i]+=triangle[j-1][i];
                 }
                 else if(i==j)
                 {
-                    dp[j][i]=dp[j-1][i-1]+triangle[j][i];
+                    triangle[j][i]+=triangle[j-1][i-1];
                 }
                 else
                 {
-                    dp[j][i]=triangle[j][i]+min(dp[j-1][i],dp[j-1][i-1]);
+                    triangle[j][i]+=min(triangle[j-1][i],triangle[j-1][i-1]);
                 }
             }
         }
@@ -25,10 +25,10 @@ public:
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         dp[0][0]=triangle[0][0];
-        fns(dp,1,n,triangle);
-        int mini=100000;
+        fns(1,n,triangle);
+        int mini=INT_MAX;
         for(int i=0;i<n;i++){
-            mini=min(dp[n-1][i],mini);
+            mini=min(triangle[n-1][i],mini);
         }
         return mini;
     }
